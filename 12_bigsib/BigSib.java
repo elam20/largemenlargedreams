94bd6f5042b5e6ae5371f6f722493b6fb9a01d68
Ethan Lam <elam20@stuy.edu>
2021-10-06 02:25:56 -0400

    init setup

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+public class BigSib {
+	public static void setHelloMsg(String m) {
+		System.out.print(m + " ");
+	}
+
+	public static String greet(String name) {
+		return(name + "\nSalutations Dr. Spaceman\nHey ya Kong Fooey\nSup mom");
+	}
+}
+

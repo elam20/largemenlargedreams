8f2d3800e8a7351b8c42af556f23950cb6e4cb61
Ethan Lam <elam20@stuy.edu>
2021-10-14 14:02:22 -0400

    sheesh

---
+++ 18_BankAccount/BankAccount.java
@@ -0,0 +1,27 @@
+public class BankAccount {
+/* 
+INSTANCE VARS
+    account holder’s full name
+    account password
+    4-digit PIN
+    9-digit account number
+    account balance
+*/
+public String PIN, Password;
+public String HolderName, AccountNum, AccountBal;
+
+/* 
+METHODS
+    setting each attribute
+    printing out all of an account’s info at once
+    depositing money
+    withdrawing money
+*/
+
+public void SetHolderName(String name) {
+HolderName = name;
+}
+
+public static void main(String[] args ){
+}
+}

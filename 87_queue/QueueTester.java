22e26f55e3390c97bc95572895e01572d5e46d9c
unknown <elam20@stuy.edu>
2022-04-04 01:17:13 -0400

    hw87

---
+++ 87_queue/QueueTester.java
@@ -0,0 +1,30 @@
+/*
+Team Won (Paul Serbanescu, Brian Wang, Ethan Lam)
+APCS
+2022--04--01
+HW87 Queues
+Time Spent: 0.3 hrs
+*/
+public class QueueTester {
+  public static void main(String[] args) {
+    ///
+    // LLQueue<Integer> queue = new LLQueue<>();
+    ALQueue<Integer> queue = new ALQueue<>();
+    ///
+
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+
+    System.out.println("peeking front: " + queue.peekFront());
+
+    System.out.println("dequeueing...");
+    System.out.println(queue.dequeue());
+    System.out.println(queue.dequeue());
+    System.out.println(queue.dequeue());
+    System.out.println(queue.dequeue());
+
+    System.out.println("is empty? " + queue.isEmpty());
+  }
+}
